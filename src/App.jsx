dcbd9d78e3108deb7313bad18128e4eac49aea58
michelebozzuto04 <michele.bozzuto04@gmail.com>
2025-07-29 11:34:25 +0200

    Add search form for movie searching functionality.

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,38 @@
+import { useState } from 'react'
 import './index.css'
 
 function App() {
 
+  const [searchInput, setSearchInput] = useState("");
+  const [results, setResults] = useState([]);
+
+  const api_key = import.meta.env.VITE_API_KEY;
+
+  function handleSearch(e) {
+    e.preventDefault()
+    fetch(`https://api.themoviedb.org/3/search/movie?api_key=${api_key}&query=${searchInput}`)
+      .then(res => res.json())
+      .then(data => setResults(data.results))
+  }
+
   return (
     <>
-      <h1>Boolflix</h1>
+      <header>
+        <form onSubmit={(e) => handleSearch(e)}>
+          <input
+            type='text'
+            value={searchInput}
+            onChange={(e) => setSearchInput(e.target.value)}
+          />
+          <button type='submit'>Search</button>
+        </form>
+
+        <ul>
+          {results.map((result) => (
+            <li>{result.title}</li>
+          ))}
+        </ul>
+      </header>
     </>
   )
 }
